608de0679898a57efe3023bd775e7d76d0fb7ef8
MaximilianHolleran <mhholleran@gmail.com>
2024-01-06 21:07:14 -0500

    sprite sheet animations working correctly!

--- README.md
+++ README.md
@@ -1,23 +1,16 @@
 # milestone-project-1
 Create a mario esque game
-start game button/ could be a href link to html file with level 1, levels 2, and 3 could be attained through collision detection of an object that hrefs you the next html file?
+
 collision detection
 Gravity implemntation 
-game has an endpoint that lets you win when you hit the flag
-game will have coins you can collect to keep score
-game will have enemies/spikes you have to avoid
-possibly add shooting to the game for bonus
-create a check of where character is at every frame
+
 
 12/19/23
 going to start from scratch 
 
-arrowleft keycode = 37
-arrowRight keycode = 39
-arrowUp keycode = 38
-arrowDown keycode = 40
+
 
 CREDITS
 Game Assets -
 Sunny Land by Anismuz on https://ansimuz.itch.io/sunny-land-pixel-game-art
-Grape man by Booty Kong on https://booty-kong.itch.io/small-animated-2d-grape-character
+
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-
-
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
 
 canvas.width = 1024
 canvas.height = 576
-
+let frameCount = 0
 const gravity = 1
+
 //player class to create the player
 class Player {
     constructor() {
@@ -14,30 +13,58 @@ class Player {
         this.position = { 
             x: 100,
             y: 100
-        }
+        };
         this.velocity = { 
             x: 0,
             y: 0
+        };
+        this.width = 62// Adjust as per your sprite sheet frame size
+        this.height =  60// Adjust as per your sprite sheet frame size
+
+        // Load sprite sheets
+        this.sprites = {
+            idleRight: { image: new Image(), frames: 3, currentFrame: 0 },
+            idleLeft: { image: new Image(), frames: 3, currentFrame: 0 },
+            runRight: { image: new Image(), frames: 4, currentFrame: 0 },
+            runLeft: { image: new Image(), frames: 4, currentFrame: 0 },
+            jump: { image: new Image(), frames: 3, currentFrame: 0 }
         }
-        this.width = 30 
-        this.height = 30
+
+        this.sprites.idleRight.image.src = './assets/idle-right.png'
+        this.sprites.idleLeft.image.src = './assets/idle-left.png'
+        this.sprites.runRight.image.src = './assets/run-right.png'
+        this.sprites.runLeft.image.src = './assets/run-left.png'
+        this.sprites.jump.image.src = './assets/jump.png'
+
+        // Current state
+        this.currentState = this.sprites.idleRight
     }
+
     draw() {
-        context.fillStyle = 'red'
-        context.fillRect(this.position.x,this.position.y,
-             this.width, this.height
-            )
+        context.drawImage(
+            this.currentState.image,
+            this.currentState.currentFrame * this.width, 1,  // Source X, Y
+            this.width, this.height, // Source width and height
+            this.position.x, this.position.y, // Canvas X, Y
+            this.width, this.height // Canvas width and height
+        );
     }
 
     update() {
         this.draw()
+
+        // Animation frame update logic
+        if (frameCount % 10 === 1) { // Adjust '10' to control speed
+            this.currentState.currentFrame = (this.currentState.currentFrame + 1) % this.currentState.frames
+        }
+
+        // Movement logic
         this.position.y += this.velocity.y
         this.position.x += this.velocity.x
-        if (this.position.y +this.height + 
-        this.velocity.y <= canvas.height)
-        this.velocity.y += gravity
         
-        
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+            this.velocity.y += gravity
+        } 
     }
 }
 //platform class to create platforms
@@ -53,17 +80,15 @@ class Platform {
        this.height = image.height  
        
     }
-
     draw(){
-        
         context.drawImage( this.image, this.position.x, this.position.y)
     }
 }
 
 let player = new Player()
 
-let backgroundImage = new Image();
-backgroundImage.src = './assets/back.png';
+let backgroundImage = new Image()
+backgroundImage.src = './assets/back.png'
 
 let image = new Image()
 image.src = './assets/platform-long.png'
@@ -72,7 +97,6 @@ image.src = './assets/platform-long.png'
 let platforms = [
 
 ]
-
 
 const keys = {
     right: {
@@ -88,8 +112,8 @@ let scrollOffset = 0
 //init function resets the game when you fall
 function init(){
 
-backgroundImage = new Image();
-backgroundImage.src = './assets/back.png';
+backgroundImage = new Image()
+backgroundImage.src = './assets/back.png'
 
 image = new Image()
 image.src = './assets/platform-long.png'
@@ -111,7 +135,11 @@ player = new Player()
     new Platform({x: 1525, y:520, image}),
     new Platform({x: 1700, y:520, image}),
     new Platform({x: 1825, y:520, image}),
-
+    new Platform({x: 2000, y:420, image}),
+    new Platform({x: 2000, y:320, image}),
+    new Platform({x: 2550, y:520, image}),
+    new Platform({x: 2670, y:520, image}),
+    new Platform({x: 2900, y:520, image}),
 ]
 
 scrollOffset = 0
@@ -120,25 +148,36 @@ scrollOffset = 0
 //adds player/platforms to screen
 function animate() {
     requestAnimationFrame(animate)
-    context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+    // Increment the frame count
+    frameCount++
+
+    // Drawing the background image
+    context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height)
+
+    // Drawing platforms
     platforms.forEach((platform) => {
         platform.draw()
     })
-    player.update() //move player update lower so it shows up in front of platforms
 
+    // Updating the player
+    player.update()
+
+    // Handling right movement
     if (keys.right.pressed && player.position.x < 400) {
-        player.velocity.x = player.speed} 
-        else if (keys.left.pressed && player.position.x > 100) {
-        player.velocity.x = -player.speed} 
-        else {player.velocity.x = 0
-//created scrolling effect
-        if (keys.right.pressed) { 
+        player.velocity.x = player.speed
+    } else if ((keys.left.pressed && player.position.x > 100) ||
+     (keys.left.pressed && scrollOffset === 0 && player.position.x > 0)) {
+        player.velocity.x = -player.speed
+    } else {
+        player.velocity.x = 0
+        // Handle scrolling effect
+        if (keys.right.pressed) {
             scrollOffset += player.speed
             platforms.forEach((platform) => {
                 platform.position.x -= player.speed
-                })
-             } 
-        else if (keys.left.pressed) {
+            })
+        } else if (keys.left.pressed && scrollOffset > 0) {
             scrollOffset -= player.speed
             platforms.forEach((platform) => {
                 platform.position.x += player.speed
@@ -146,82 +185,77 @@ function animate() {
         }
     }
 
-    
-    //platform colision detection
+    // Platform collision detection
     platforms.forEach((platform) => {
-    if (
-        player.position.y + player.height <=
-            platform.position.y &&
-         player.position.y + player.height +
-            player.velocity.y >= 
-            platform.position.y && 
-        player.position.x + player.width >= 
-        platform.position.x && 
-        player.position.x <= platform.position.x + platform.width
-        ){
+        if (player.position.y + player.height <= platform.position.y &&
+            player.position.y + player.height + player.velocity.y >= platform.position.y &&
+            player.position.x + player.width >= platform.position.x &&
+            player.position.x <= platform.position.x + platform.width) {
             player.velocity.y = 0
         }
     })
-    //win condition
+
+    // Win condition
     if (scrollOffset > 1000) {
         console.log('you win')
     }
-    //lose condition
+
+    // Lose condition
     if (player.position.y > canvas.height) {
         init()
     }
 }
 
-init()
-animate()
+
 // keydown controls to move
-addEventListener('keydown', ({ keyCode }) =>{
-    
+addEventListener('keydown', ({ keyCode }) => {
     switch (keyCode) {
-        case 37:
+        case 37: // Left arrow
             console.log('left')
-            keys.left.pressed = true
+            keys.left.pressed = true;
+            player.currentState = player.sprites.runLeft // Switch to running left animation
+            player.currentState.currentFrame = 0 // Reset animation frame
             break
 
-        case 39:
+        case 39: // Right arrow
             console.log('right')
             keys.right.pressed = true
+            player.currentState = player.sprites.runRight; // Switch to running right animation
+            player.currentState.currentFrame = 0 // Reset animation frame
             break
 
-        case 32:
+        case 32: // Spacebar
             console.log('up')
-            player.velocity.y -= 10
+            player.velocity.y -= 15
+            player.currentState = player.sprites.jump // Switch to jumping animation
+            player.currentState.currentFrame = 0 // Reset animation frame
             break
 
-        case 40:
-            console.log('down')
-            break
     }
-    
 })
 //keyup controls to stop moving
-addEventListener('keyup', ({ keyCode }) =>{
-    
+addEventListener('keyup', ({ keyCode }) => {
     switch (keyCode) {
-        case 37:
+        case 37: // Left arrow
             console.log('left')
             keys.left.pressed = false
-            break
+            player.currentState = player.sprites.idleLeft // Switch to idle left animation
+            player.currentState.currentFrame = 0 // Reset animation frame
+            break;
 
-        case 39:
+        case 39: // Right arrow
             console.log('right')
             keys.right.pressed = false
-            break
+            player.currentState = player.sprites.idleRight // Switch to idle right animation
+            player.currentState.currentFrame = 0 // Reset animation frame
+            break;
 
-        // case 32:
-        //     console.log('up')
-        //     player.velocity.y -= 8
-        //     break 
-
-        case 40:
-            console.log('down')
+        case 32: // Space bar
+            console.log('up')
+            // Decide the idle state based on the last direction of movement
             break
     }
-    
-
 })
+
+init()
+animate()
